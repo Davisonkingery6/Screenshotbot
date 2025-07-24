96ca854eacd11fe802095a54e89383840e1d724b
Davisonkingery6 <128478790+Davisonkingery6@users.noreply.github.com>
2025-07-24 16:12:08 +0100

    Update Screenshot_Visual_Loop.bat

--- Screenshot_Visual_Loop.bat
+++ Screenshot_Visual_Loop.bat
@@ -1,12 +1,12 @@
 @echo off
-title Screenshot Bot - Screen 2 [Visual Tracker]
+title Screenshot Bot - Screen 2 [24H Fixed]
 mode con: cols=60 lines=25
 color 0A
 setlocal enabledelayedexpansion
 
 echo ============================================
 echo    SCREENSHOT BOT - SCREEN 2
-echo    Target Times: 4PM, 5PM, 6PM, 7PM
+echo    Target Times: 16:00, 17:00, 18:00, 19:00
 echo ============================================
 echo.
 
@@ -27,25 +27,24 @@ echo  Current Date: %date%
 echo.
 echo  SCHEDULE STATUS:
 echo  ----------------
-echo  [4:00 PM] - %shot1%
-echo  [5:00 PM] - %shot2%
-echo  [6:00 PM] - %shot3%
-echo  [7:00 PM] - %shot4%
+echo  [16:00] - %shot1%
+echo  [17:00] - %shot2%
+echo  [18:00] - %shot3%
+echo  [19:00] - %shot4%
 echo.
 echo  Total Captured Today: %total% / 4
 echo.
 echo ============================================
 
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do (
-    set currenttime=%%a:%%b
-)
+set currenttime=%time:~0,5%
 
-if "%currenttime%"=="04:00 PM" if "%shot1%"=="NO" goto capture1
-if "%currenttime%"=="05:00 PM" if "%shot2%"=="NO" goto capture2
-if "%currenttime%"=="06:00 PM" if "%shot3%"=="NO" goto capture3
-if "%currenttime%"=="07:00 PM" if "%shot4%"=="NO" goto capture4
+if "%currenttime%"=="16:00" if "%shot1%"=="NO" goto capture1
+if "%currenttime%"=="17:00" if "%shot2%"=="NO" goto capture2
+if "%currenttime%"=="18:00" if "%shot3%"=="NO" goto capture3
+if "%currenttime%"=="19:00" if "%shot4%"=="NO" goto capture4
 
 echo  STATUS: Waiting for next scheduled time...
+echo  Current time: %currenttime%
 echo.
 echo  Next Check in: 30 seconds
 timeout /t 30 /nobreak >nul
